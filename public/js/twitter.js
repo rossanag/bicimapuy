b280b0324f8df6c545d5b33085e1b6084f6086b2
rossanag <guerra.rossana@gmail.com>
2016-04-23 22:35:44 -0300

    version release 1.0 beta

--- public/js/twitter.js
+++ public/js/twitter.js
@@ -1,11 +1,1 @@
-// Requiere JQuery
-var show_twitter = false;
-
-
-function handleTwitter()
-{
-	if (show_titter)
-		$('#twitter').hide();	
-	else
-		$('#twitter').show();		
-}
+function handleTwitter(){show_titter?$("#twitter").hide():$("#twitter").show()}var show_twitter=!1;
