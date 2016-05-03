43c099b81935bc57115529fd9670c19c23b0ffbe
rossanag <guerra.rossana@gmail.com>
2016-05-03 05:11:08 -0300

    version releaste 1.0 beta bicimapuy

---
+++ public/Recorridos/recorridos.js
@@ -0,0 +1,28 @@
+{"Recorridos":
+ [
+  {"Rapidos":
+   [
+    {"Nombre":"CordonSayago.js","Ruta":"Cordón a Sayago","Longitud":"10.53 km","Desc":"Va principalmente por Avenida Millán.\n Calles en buen estado general, al ser avenidas y calles transitadas hay también una relativa seguridad","lat":-34.83747716241598,"lng":-56.204681396484375},             
+    {"Nombre":"sayagoCordon.js","Ruta":"Sayago a Cordón", "Longitud":"10.31 kms", "Desc":"Va principalmente por Avenida Millán.\n Calles en buen estado general, al ser avenidas y calles transitadas hay también una relativa seguridad","lat": -34.83846342495163,"lng":-56.20227813720703},
+    {"Nombre":"CasaFing.js","Ruta":"Reducto a Fac. de Ingeniería", "Longitud":"5.74 km", "Desc":"Recorrido accesible, sin demasiados repechos\n Aprox. 20 minutos","lat":-34.888324718606846, "lng":-56.17575645446777},    
+    {"Nombre":"toledoFing.js","Ruta":"Toledo a Fac. de Ingeniería", "Longitud":"24.07 km", "Desc":"Recorrido directo","lat":-34.8084475751538, "lng":-56.12091064453125},
+    {"Nombre":"prcv.js","Ruta":"Pque. Rodó a Ciudad Vieja", "Longitud":"3.52 km", "Desc":"Rápido y seguro, de dificultad baja.\n Evita la rambla en horas pico\n.Al llegar a Reconquista se puede tomar la ciclovía (doble sentido).\n Hay 2 repechos importantes: la cuadra antes de llegar a Ejido, y otro al tomar Wilson Ferreira Aldunate. Aprox. 20 minutos (ritmo tranquilo)"
+,"lat":-34.909724809310575,"lng":-56.181678771972656},    
+    {"Nombre":"cvpr.js","Ruta":"Ciudad Vieja a Pque. Rodó", "Longitud":"5 km", "Desc":"Rápido y seguro, de dificultad baja.\n En horas pico amerita las cuadras hasta Zabala para evitar Buenos Aires y tomar\n  la ciclovía de Reconquista (doble sentido). Va por Domingo Petraca (1 cuadra en estado regular) para \n tomar Cebollatí y evitar el tráfico de Av. Gonzalo Ramírez\n.Tiene 3 repechos moderados: en Maldonado, otro en Durazno y en Lauro Muller antes de Jackson. Aprox. 20 minutos (ritmo tranquilo).", "lat":-34.90859862776119,"lng":-56.18906021118164},    
+    {"Nombre":"cViejaPrado_rapido2.js","Ruta":"Ciudad Vieja a Prado", "Longitud":"7.09 km", "Desc":"Tiene algunos tramos seguros, otros de alto riesgo pero lo más directo y llanos posibles","lat":-34.878326713257394,"lng":-56.19386672973632},    
+    {"Nombre":"pradoCVieja_rapido2.js","Ruta":"Prado a Ciudad Vieja", "Longitud":"7.44 km", "Desc":"Tiene algunos tramos seguros, otros de alto riesgo pero lo más directo y llanos posibles","lat":-34.88212891495235,"lng":-56.19163513183594},
+    {"Nombre":"pqeRodoPrado_rapido2.js","Ruta":"Pque. Rodó a Prado", "Longitud":"9.03 km", "Desc":"Tiene algunos tramos seguros, otros de alto riesgo pero lo más directo y llanos posibles","lat":-34.88635337720632,"lng":-56.17807388305663},
+    {"Nombre":"cViejaPocitos_rapido2.js","Ruta":"Ciudad Vieja a Pocitos", "Longitud":"5.18 km", "Desc":"Tiene algunos tramos seguros, otros de alto riesgo pero lo más directo y llanos posibles","lat":-34.903812183845474,"lng":-56.181678771972656},				
+    {"Nombre":"pocitosCVieja_rapido2.js","Ruta":"Pocitos a Ciudad Vieja", "Longitud":"5.58 km", "Desc":"Tiene algunos tramos seguros, otros de alto riesgo pero lo más directo y llanos posibles","lat":-34.907542818529386,"lng":-56.17541313171387}				        
+   ]
+  },
+  {"Seguros":
+   [
+    {"Nombre":"prcv.js","Ruta":"Pque. Rodó a Ciudad Vieja", "Longitud":"", "Desc":""},    
+    {"Nombre":"cvpr.js","Ruta":"Ciudad Vieja a Pque. Rodó", "Longitud":"", "Desc":""}    
+   ]
+  },
+  {"Turísticos":[]
+  }
+ ]
+}
