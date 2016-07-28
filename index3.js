ed77a675d910f372acd9b6a53d1cd2e2cbd54d58
rossanag <guerra.rossana@gmail.com>
2016-07-28 01:53:19 -0300

    con certificado SSL

--- index3.js
+++ index3.js
@@ -21,6 +21,17 @@ app.use( express.static(__dirname + '/public'));
 app.use(function(req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-Width");
+  // Nuevo
+
+  // if (req.header('x-forwarded-proto') == 'http') {
+  //   res.redirect(301, 'https://' + 'www.bicumap.uy' + req.url);
+  //   return;
+  // }
+  //next();
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+  next();
 }
   )
 // Redireccionamiento a https
@@ -42,6 +53,7 @@ app.use(function(req,res,next) {
                   res.redirect('https://' + req.headers.host + req.url);
           }
   });
+
 
 
 var arrayRecorridos = [
