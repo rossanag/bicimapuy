65ffc79894be229439a9a1728251f170b31e0dad
rossanag <guerra.rossana@gmail.com>
2016-10-22 02:03:39 -0300

    agregando opciones de ruteo

---
+++ public/Menu/js/menuMax.js
@@ -0,0 +1,128 @@
+$(function() {
+				//$('nav#menu').mmenu();
+				menu =  $('nav#menu').mmenu(
+
+				{
+					navbars		: [{
+						position: "top",
+						height 	: 1,
+						content : [
+
+							'<a class="fa fa-info-circle fa-2x"></a>'
+
+						]
+					},
+					{
+			            position: "bottom", //bottom
+	        		    content: [
+	        		    	'<a href="mailto:bicimapuy@gmail.com" class="fa fa-envelope"></a>',
+	               			'<a class="fa fa-twitter"></a>',
+	               			'<a href="https://www.facebook.com/" target="_blank"class="fa fa-facebook"></a>'
+	            		]
+         			}
+					],
+					extensions: ["multiline"],
+        			 onClick: {
+             			close: false
+        			 },
+        			navbar:{
+        				title: "Inicio"
+        			},
+        			/* offCanvas: {
+                		  zposition : "next"
+             		}, */
+             		clone: true
+
+			});
+
+			API = $("#menu").data( "mmenu" ); // antes var API
+
+			menu.on( 'click', 'a[class^="fa fa-twitter"]', function() {  // esto funciona
+			 				//$('#twitter').show();
+							$( "#twitter" ).toggle();
+      						  //var API = $("#menu").data( "mmenu" );
+       						  API.close();
+
+			 				return false;
+			 			}
+			 );
+			 menu.on( 'click', 'a[class^="fa fa-info-circle"]', function() {  // esto funciona
+							if ($( "#dialogo" ).dialog( "isOpen" ))
+								$( "#dialogo" ).dialog( "close" );
+							else
+								$( "#dialogo" ).dialog( "open" );
+
+
+			 				return false;
+			 			}
+			 );
+
+			$( "#dialogo" ).dialog({
+  				autoOpen: false,
+  				position: { my: "right top", at: "right top", of: window }
+			});
+			$('#loading').hide();
+
+        });
+
+		$( window ).load(function() {
+
+  			var $ruteo = $('input[name="ruteo"]');
+  			$ruteo.click(function() {
+  				borrarPath();
+	  			if ($ruteo.is( ':checked' ))
+	  			{
+	  					routing = true;
+	      				displayRoutingControl();
+	      				//API.close();
+	  			}
+	  			else
+	  			{
+	  				borrarRuta();
+	  				removeRouting();
+
+	  			}
+
+  			});
+
+				var $_aPie = $('input[id="peaton"]');
+				var $_enBici = $('input[id="enBici"]');
+				var $_enAuto = $('input[id="enAuto"]');
+
+				$_aPie.click(function() {
+					if (routeControl != null)
+					{
+						if ($_aPie.is( ':checked' )){
+							routeControl.router.route({costing: "pedestrian"});
+							//borrarRuta();
+						}
+					}
+
+				});
+				$_enBici.click(function() {
+					if (routeControl != null)
+					{
+						if ($_enBici.is( ':checked' ))
+							routeControl.router.route({costing: "bycicle"});
+							//borrarRuta();
+					}
+					else {
+						alert("es nulo el control");
+					}
+				});
+				$_enAuto.click(function() {
+					if (routeControl != null)
+					{
+						if ($_enAuto.is( ':checked' ))
+							routeControl.router.route({costing: "auto"});
+							borrarRuta();
+					}
+
+				});
+
+      		API.close();
+
+  			});
+
+
+//});
